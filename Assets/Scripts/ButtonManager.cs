3bf050e7b4900f6120981b733f332a25131cbb40
Louis Cheung <dragonxii2002@gmail.com>
2021-02-27 16:30:23 +0800

    Update UI Button

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ButtonManager : MonoBehaviour
 {
@@ -15,11 +16,20 @@ public class ButtonManager : MonoBehaviour
         btn.onClick.AddListener(SelectObject);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (UIManager.Instance.OnEntered(gameObject))
+        {
+            transform.DOScale(Vector3.one * 2, 0.2f);
+            //transform.localScale = Vector3.one * 2;
+        }
+        else
+        {
+            transform.DOScale(Vector3.one, 0.2f);
+            //transform.localScale = Vector3.one;
+        }
     }
+
 
     void SelectObject()
     {
